5fb52f8db6fc2e297b7c71cab3ce41656c0049d9
lzqiang <335892083@qq.com>
2016-11-03 16:16:20 +0800

    sql

---
+++ 事故信息final.sql
@@ -0,0 +1,177 @@
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(1,-1,'事故灾难',1,'',SYSDATE);
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(2,-1,'煤矿事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(3,2,'事故灾难',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(4,2,'煤矿瓦斯事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(5,2,'煤矿顶板事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(6,2,'煤矿运输事故 ',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(7,2,'煤矿水害事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(8,2,'煤矿机电事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(9,2,'煤矿放炮事故',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(10,2,'煤矿火灾事故',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(11,2,'煤矿其他事故',8,'',SYSDATE);
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(12,-1,'金属与非金属矿山事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(13,12,'金属与非金属矿顶板事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(14,12,'金属与非金属矿水害事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(15,12,'金属与非金属矿中毒和窒息事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(16,12,'金属与非金属矿尾矿库垮坝事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(17,12,'金属与非金属矿火灾事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(18,12,'金属与非金属矿机电事故',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(19,12,'金属与非金属矿运输事故',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(20,12,'金属与非金属矿放炮事故',8,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(21,12,'金属与非金属矿火药爆炸事故',9,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(22,12,'金属与非金属矿山其他事故',10,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(23,-1,'建筑业事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(24,23,'房屋建筑与市政工程施工安全事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(25,23,'其他建筑施工安全事故',2,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(26,-1,'危险化学品事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(27,26,'危险化学品爆炸事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(28,26,'危险化学品泄漏事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(29,26,'危险化学品火灾事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(30,26,'危险化学品中毒和窒息事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(31,26,'危险化学品灼烫事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(32,26,'危险化学品其他事故',6,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(33,-1,'烟花爆竹和民用爆炸物事故',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(34,33,'烟花爆竹生产企业爆炸事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(35,26,'烟花爆竹运输爆炸事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(36,33,'民用爆炸物爆炸事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(37,33,'其他烟花爆竹事故',4,'',SYSDATE);
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(38,-1,'其他工矿商贸事故',7,'',SYSDATE);
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(39,-1,'火灾事故',8,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(40,39,'一般工业建筑火灾',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(41,39,'特种工业建筑火灾',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(42,39,' 一般民用建筑火灾',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(43,39,'高层民用建筑火灾',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(44,39,'地下建筑火灾',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(45,39,'公用建筑火灾',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(46,39,'隧道火灾',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(47,39,'其它火灾事故',8,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(48,-1,'道路交通事故',9,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(49,48,'翻车事件',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(50,48,'撞车事件',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(51,48,'车辆坠水、坠沟事件',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(52,48,'车辆起火事件',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(53,48,'校车交通事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(54,48,'其他道路交通事故',6,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(55,-1,'水上交通事故',10,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(56,55,' 船舶碰撞事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(57,55,' 船舶触礁事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(58,55,'  船舶触损事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(59,55,' 船舶搁浅事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(60,55,' 船舶遭受风灾事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(61,55,' 船舶火灾事故',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(62,55,' 船舶失踪事故',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(63,55,' 船舶海上遇险事故',8,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(64,55,' 水上保安事件',9,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(65,55,' 沿海渔业设施事故',10,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(66,55,' 其他水上交通事故',11,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(67,-1,'铁路交通事故',11,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(68,67,'  列车脱轨事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(69,67,'  列车追尾事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(70,67,'   列车撞车事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(71,67,'  列车撞人事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(72,67,' 列车火灾、爆炸事故 ',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(73,67,' 其他铁路交通事故 ',6,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(74,-1,'城市轨道交通事故',12,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(75,74,'  地铁、轻轨、单轨列车脱轨事故 ',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(76,74,'  地铁、轻轨、单轨列车追尾事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(77,74,'   地铁、轻轨、单轨列车撞车事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(78,74,'  地铁、轻轨、单轨列车撞人事故 ',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(79,74,' 地铁、轻轨、单轨列车火灾、爆炸事故  ',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(80,74,'  	其他城市轨道交通事故  ',6,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(81,-1,'民用航空器事故',13,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(82,81,'坠机事件',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(83,81,'撞机事件 ',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(84,81,'其他民用航空器飞行事故',3,'',SYSDATE);
+
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(85,-1,'特种设备事故',14,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(86,85,'  锅炉事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(87,85,'  压力容器事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(88,85,'   压力管道事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(89,85,'  电梯事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(90,85,'  起重机械事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(91,85,'  客运索道事故',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(92,85,'  大型游乐设施事故',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(93,85,'  其他特种设备事故',8,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(94,-1,'基础设施和公用设施事故',15,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(95,94,'公路交通设施事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(96,94,'铁路交通设施事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(97,94,'城市轨道交通设施事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(98,94,'城市桥梁隧道设施事故',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(99,94,'水运交通设施事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(100,94,'民航交通设施事故',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(101,94,'水利设施事故',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(102,94,'电力基础设施事故',8,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(103,94,'石油天然气基础设施事故',9,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(104,94,'通讯基础设施事故',10,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(105,94,'金融基础设施事故',11,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(106,94,'城市生命线基础设施事故',12,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(107,94,'建筑垮塌事故',13,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(108,94,'其他公用设施和设备事故',14,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(109,-1,'环境污染和生态破坏事件',16,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(110,109,' 水域污染事件',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(111,109,' 空气污染事件',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(112,109,' 土壤污染事件',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(113,109,' 海上溢油事件',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(114,109,' 污染导致城市水源供水中断事故',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(115,109,' 转基因生物生态破坏事件',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(116,109,'盗伐、滥伐、哄抢森林事件 ',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(117,109,'毁林、乱占林地、非法改变林地用途事件',8,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(118,109,'濒危物种生存环境遭受环境污染事件 ',9,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(119,109,'野生动（植）物种群大批死亡事件 ',10,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(120,109,'自然保护区、风景名胜区生态破坏',11,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(121,109,'进口再生原料污染事件 ',12,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(122,109,'非法倾倒、埋藏剧毒危险废物事件',13,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(123,109,'其它环境污染和生态破坏事件 ',14,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(124,-1,'农业机械事故',17,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(125,124,'  农业机械行驶事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(126,124,'  农业机械作业事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(127,109,'  农业机械碾压事件',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(128,124,'  农业机械碰撞事件',4,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(129,124,'  农业机械翻车事件',5,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(130,124,'  农业机械落车事件',6,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(131,124,' 农业机械火灾事件 ',7,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(132,124,' 其他农业机械事故',8,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(133,-1,'踩踏事件',18,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(134,133,'   公园组织大型群众性活动或聚会踩踏事件',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(135,133,'   校园踩踏事件',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(136,133,'   其他踩踏事件',3,'',SYSDATE);
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(137,-1,'核与辐射事故',18,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(138,137,'   核设施事故',1,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(139,137,'   放射性物质运输事故',2,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(140,137,'   放射源事故',3,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(141,137,'   射线装置事故',4,'',SYSDATE);
+
+
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(142,-1,'能源供应中断事故',19,'',SYSDATE);
+INSERT INTO ems_accident_info (id,pid,NAME,SORT,notes,updatetime) VALUES(143,-1,'其他事故灾难',20,'',SYSDATE);
